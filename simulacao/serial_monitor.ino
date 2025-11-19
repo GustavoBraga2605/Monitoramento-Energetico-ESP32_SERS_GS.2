24cdc693da0e18b619562a10ea0125ea8386f37e
GustavoBraga2605 <guuubraga@gmail.com>
2025-11-19 13:05:46 -0300

    Create serial_monitor.ino

---
+++ simulacao/serial_monitor.ino
@@ -0,0 +1,28 @@
+# Dados do serial monitor em uma simulação do projeto dentro do Wokwi.
+# Os dados foram divididos em 3 partes:
+# Luminosidade BAIXA = 49 -- 999
+# Luminosidade MÉDIA = 1000 -- 2999
+# Luminosidade ALTA = 3000 -- 4063
+  
+Sistema de monitoramento de luminosidade iniciado.
+
+LDR = 49 // Luminosidade BAIXA
+LDR = 74 // Luminosidade BAIXA
+LDR = 104 // Luminosidade BAIXA
+LDR = 448 // Luminosidade BAIXA
+LDR = 680 // Luminosidade BAIXA
+LDR = 905 // Luminosidade BAIXA
+
+LDR = 1207 // Luminosidade MÉDIA
+LDR = 1437 // Luminosidade MÉDIA
+LDR = 1718 // Luminosidade MÉDIA
+LDR = 2340 // Luminosidade MÉDIA
+LDR = 2653 // Luminosidade MÉDIA
+LDR = 2884 // Luminosidade MÉDIA
+
+LDR = 3067 // Luminosidade ALTA
+LDR = 3295 // Luminosidade ALTA
+LDR = 3632 // Luminosidade ALTA
+LDR = 3797 // Luminosidade ALTA
+LDR = 3923 // Luminosidade ALTA
+LDR = 4063 // Luminosidade ALTA
